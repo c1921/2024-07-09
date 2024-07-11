69b97ddb5c60a8aaf2e6a9f9a10123d62e402e88
北邙 <communism_1921@163.com>
2024-07-11 14:35:35 +0800

    修改快捷键

--- shortcuts.py
+++ shortcuts.py
@@ -1,12 +1,10 @@
-# shortcuts.py
-
 from PyQt6.QtGui import QKeySequence
 from PyQt6.QtCore import Qt
 
 class Shortcuts:
     PAUSE_CONTINUE = QKeySequence(Qt.Key.Key_Space)
-    SPEED_1X = QKeySequence(Qt.Key.Key_1)
-    SPEED_2X = QKeySequence(Qt.Key.Key_2)
-    SPEED_5X = QKeySequence(Qt.Key.Key_3)
-    SPEED_10X = QKeySequence(Qt.Key.Key_4)
+    SPEED_1X = QKeySequence(Qt.Key.Key_F1)
+    SPEED_2X = QKeySequence(Qt.Key.Key_F2)
+    SPEED_5X = QKeySequence(Qt.Key.Key_F3)
+    SPEED_10X = QKeySequence(Qt.Key.Key_F4)
     SWITCH_TAB = QKeySequence(Qt.Key.Key_Tab)
